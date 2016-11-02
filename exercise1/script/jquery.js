cbae4300eac776410bcc3248380bf11e6e084d01
Mary Scale <scalemary@gmail.com>
2016-11-02 21:25:24 +0000

    yesgit add exercise1! managed to show and hide the buttons, just needed to append two of them then show hide them alternatively. Not certain that this was the desired solution, but it works infinitely without messing with the classes

--- exercise1/script/jquery.js
+++ exercise1/script/jquery.js
@@ -1,5 +1,31 @@
 var $ = $;
 
 $(document).ready(function(){
-    $('.announcement').append('<button>X</button>');
+    $('.announcement').append('<button class="hide">X</button>');
+    $('.announcement').append('<button class="show">show announcements</button>');
+        // Create two buttons
+        
+    $(".show").hide();
+        // hide the show button
+    
+    $('.hide').click(function(){
+        
+        $(".announcement p").fadeOut('slow', function(){
+        });
+        
+        $('.show').show();
+        $('.hide').hide();
+    });
+        // hide button click
+    
+    $('.show').click(function(){
+        $(".announcement p").fadeIn('slow', function(){
+            $('.show').hide();
+            $('.hide').show();
+            
+        });
+    })
+    
+    
+    
 })
