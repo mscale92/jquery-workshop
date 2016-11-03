4e1eb6e9943a0e36cb2d3d2bf5e9a92375637f75
Mary Scale <scalemary@gmail.com>
2016-11-03 16:41:12 +0000

    added a marker

--- exercise6/script/jquery.js
+++ exercise6/script/jquery.js
@@ -7,8 +7,13 @@ $(document).ready(function(){
         console.log(res.results[0].geometry.location);
         var lonLat = res.results[0].geometry.location;
         
+        var coordinates = lonLat.lat + "," + lonLat.lng;
+        
+        console.log(coordinates)
+        
         var url = "https://maps.googleapis.com/maps/api/staticmap?center="
-        + lonLat.lat + "," + lonLat.lng + "&zoom=13&size=600x300&maptype=hybrid"
+        + coordinates + "&zoom=13&size=600x300&maptype=hybrid"
+        + "&markers=color:blue%7Clabel:S%7C" + coordinates
         + "&key=AIzaSyD9Fness26-tcuA4khBN6molgSPoFfWzN4";
             // make the url for ISS
         
