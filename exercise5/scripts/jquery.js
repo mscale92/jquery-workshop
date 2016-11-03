bbc1faea5f93b832cdd142b859f387f7d1a085c1
Mary Scale <scalemary@gmail.com>
2016-11-03 15:53:12 +0000

    made my ISS webpage look a little zanny, but nicely centered

--- exercise5/scripts/jquery.js
+++ exercise5/scripts/jquery.js
@@ -1,8 +1,19 @@
 var $ = $;
-
+var jQuery = jQuery;
 
 $(document).ready(function(){
 
- 
+ jQuery.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=montreal').then(
+    function(res) {
+        console.log(res.results[0].geometry.location);
+        var lonLat = res.results[0].geometry.location;
+        
+        $("h2").after("<p> ISS has a latitude of " + lonLat.lat + " and a longitude of " + lonLat.lng + "</p>");
+    }
+)
+.catch(function(err){
+ alert("oh dear, something went wrong. Try again later");
+ console.log("there was an error ", err);
+})
  
 }) 
