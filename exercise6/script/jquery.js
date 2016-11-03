b60d2d5b89d276d3aa2b7f36f2f603281f063325
Mary Scale <scalemary@gmail.com>
2016-11-03 17:02:34 +0000

    accidentally was using a google address, oops, now using where the iss api to track in realtime. Also changed the scope of the map, the zoom, to continent level. That way it shows the oceans

--- exercise6/script/jquery.js
+++ exercise6/script/jquery.js
@@ -2,27 +2,42 @@ var $ = $;
 var jQuery = jQuery;  
   
 $(document).ready(function(){
- jQuery.getJSON('https://maps.googleapis.com/maps/api/geocode/json?address=iss').then(
-    function(res) {
-        console.log(res.results[0].geometry.location);
-        var lonLat = res.results[0].geometry.location;
+    locateISS();
+        // run the iss locater function
         
-        var coordinates = lonLat.lat + "," + lonLat.lng;
+    window.setInterval(locateISS, 10000);
+        // update location every ten seconds
         
-        console.log(coordinates)
         
-        var url = "https://maps.googleapis.com/maps/api/staticmap?center="
-        + coordinates + "&zoom=13&size=600x300&maptype=hybrid"
-        + "&markers=color:blue%7Clabel:S%7C" + coordinates
-        + "&key=AIzaSyD9Fness26-tcuA4khBN6molgSPoFfWzN4";
-            // make the url for ISS
-        
-        $("#iss-position").attr('src', url);
-    }
-)
-.catch(function(err){
-    console.log("There was an error: ", err);
-    alert("Oh dear, there seems to be an error. Please try again later.")
-})
+function locateISS() {
+    
+    
  
+     jQuery.getJSON('https://api.wheretheiss.at/v1/satellites/25544').then(
+        function(res) {
+            console.log(res);
+            
+            
+            var coordinates = res.latitude + "," + res.longitude;
+            
+            console.log(coordinates)
+            
+            var url = "https://maps.googleapis.com/maps/api/staticmap?center="
+            + coordinates + "&zoom=5&size=600x300&maptype=hybrid"
+            + "&markers=color:blue%7Clabel:S%7C" + coordinates
+            + "&key=AIzaSyD9Fness26-tcuA4khBN6molgSPoFfWzN4";
+                // make the url for ISS
+            
+            $("#iss-position").attr('src', url);
+            
+            
+        }
+    
+        )
+        .catch(function(err){
+            console.log("There was an error: ", err);
+            alert("Oh dear, there seems to be an error. Please try again later.")
+        })
+}
+//end of locateISS function
 }) 
